92b8e1601fc3e2384de4367dca2ac364fbf4d233
Adilson Oliveira <aquatroolhos@gmail.com>
2020-07-14 16:40:39 -0300

    logo do readme

--- src/screens/ConsultTask.js
+++ src/screens/ConsultTask.js
@@ -8,8 +8,29 @@ export default class ConsultTask extends Component {
     render(){
         return(
             <Modal transparent={true}>
+                <TouchableWithoutFeedback
+                    onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
+                <View>
+
+                </View>                
+                <TouchableWithoutFeedback
+                    onPress={this.props.onCancel}>
+                    <View style={styles.background}></View>
+                </TouchableWithoutFeedback>
             </Modal>
         )
     }
 
 }
+
+const styles = StyleSheet.create({
+    background:{
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.7)'
+    },
+    container: {
+        backgroundColor:'#FFF'
+    }
+})
